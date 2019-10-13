7c707902aaed22f71afdd6934bcf03f0026be9d9
farrellw <farrell.will.g@gmail.com>
2019-10-13 16:13:50 -0500

    Add typescript runner and gitignore.

---
+++ runner.ts
@@ -0,0 +1,27 @@
+// JavaScript declaring a variable
+const myCatsName = "Garfield";
+
+// TypeScript declaring a variable
+const myDogsName: string = "Shayla";
+
+// A function that takes the average of two input parameters.
+// Strongly type the inputs and outputs
+// What happens when providing the wrong type to the function?
+
+// Write a function that returns a dog age from a human age.
+// age. humanYears, numbers
+
+// Write a function, isOdd, which returns true if the number passed in is not divisible by 2
+
+// Write a JavaScript function that accepts a string as a parameter and returns a string, where the first letter of each word of the string in upper case.
+// Example string : 'the quick brown fox'
+// Expected Output : 'The Quick Brown Fox '
+
+// Complex types:
+type Animal = {
+  name: string;
+  age: number;
+};
+
+// Array of Objects
+const myAnimals: Animal[] = [];
